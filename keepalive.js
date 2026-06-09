d0caea5221a2f115eba8c2044fa3908dc525df45
jayesh-borase <jayeshborse370@gmail.com>
2026-06-10 02:50:08 +0530

    Enable manual workflow and some file modifeid 3

--- keepalive.js
+++ keepalive.js
@@ -3,12 +3,13 @@ const mysql = require("mysql2/promise");
 async function pingDB() {
   const conn = await mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     ssl: {
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   });
 
   await conn.query("SELECT 1");
