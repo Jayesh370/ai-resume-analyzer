22c1977ade438dc36ac34d9157fd80777a599cc6
jayesh-borase <jayeshborse370@gmail.com>
2026-06-10 02:42:22 +0530

    added some file in proudction level fixing login issue with base urls 2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,7 +6,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,          // Vite dev proxy → http://localhost:5000/api
+  baseURL: "https://ai-resume-analyzer-x8un.onrender.com/api",          // Vite dev proxy → http://localhost:5000/api
   timeout: 30000,           // 30 s (AI calls can be slow)
   headers: { "Content-Type": "application/json" },
 });
